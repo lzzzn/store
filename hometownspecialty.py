b3fa81e7894871ab06b70a70726784c64421cbdf
lzzzn <86229194+lzzzn@users.noreply.github.com>
2021-06-22 19:02:15 +0800

    Add files via upload

---
+++ hometownspecialty.py
@@ -0,0 +1,46 @@
+
+print ("----------------家乡特产------------------")
+
+print ("          名称          价格          品质          产地")
+id1 = 1
+name1="哈尔滨红肠"
+price1=40
+quan="A+"
+add="黑龙江"
+
+print (name1,"\t",price1,"\t\t\t",quan,"\t\t\t",add,)
+
+id2 = 2
+name2="木耳"
+price2=15
+quan="A+"
+add="黑龙江"
+
+print (name2,"\t\t",price2,"\t\t\t",quan,"\t\t\t",add,)
+
+id3 = 3
+name3="冻梨"
+price3=15
+quan="A+"
+add="黑龙江"
+print (name3,"\t\t",price3,"\t\t\t",quan,"\t\t\t",add,)
+
+id4 = 4
+name4="蓝莓"
+price4=58
+quan="A+"
+add="大兴安岭"
+
+print (name4,"\t\t",price4,"\t\t\t",quan,"\t\t\t",add,)
+
+id5 = 5
+name5="杀猪菜"
+price5=40
+quan="A+"
+add="黑龙江"
+
+print (name5,"\t\t",price5,"\t\t\t",quan,"\t\t\t",add,)
+
+
+
+
---
+++ shop.py
@@ -0,0 +1,265 @@
+
+print ("----------------京城小卖铺------------------")
+
+print ("日期          名称          价格          库存数量          销售量")
+id1 = 1
+date="1号"
+name1="羽绒服"
+price1=253.6
+num=500
+num1=10
+
+print (date,"\t\t",name1,"\t\t",price1,"\t\t\t",num,"\t\t\t",num1,)
+
+id2 = 2
+date="2号"
+name2="牛仔裤"
+price2=86.3
+num=600
+num2=60
+
+print (date,"\t\t",name2,"\t\t",price2,"\t\t\t",num,"\t\t\t",num2,)
+
+id3 = 3
+date3="3号"
+name3="风衣"
+price3=96.8
+num=335
+num3=43
+print (date,"\t\t",name3,"\t\t",price3,"\t\t\t",num,"\t\t\t",num3,)
+
+id4 = 4
+date="4号"
+name4="皮草"
+price4=135.9
+num=855
+num4=63
+
+print (date,"\t\t",name4,"\t\t",price4,"\t\t\t",num,"\t\t\t",num4,)
+
+id5 = 5
+date="5号"
+name5="T恤"
+price5=65.8
+num=632
+num5=63
+
+print (date,"\t\t",name5,"\t\t",price5,"\t\t\t",num,"\t\t\t",num5,)
+
+id6 = 6
+date="6号"
+name6="衬衫"
+price6=49.3
+num=562
+num6=120
+
+print (date,"\t\t",name6,"\t\t",price6,"\t\t\t",num,"\t\t\t",num6,)
+
+id7 = 7
+date="7号"
+name7="牛仔裤"
+price7=86.3
+num=600
+num7=60
+print (date,"\t\t",name7,"\t\t",price7,"\t\t\t",num,"\t\t\t",num7,)
+
+id8 = 8
+date="8号"
+name8="羽绒服"
+price8=253.6
+num=500
+num8=10
+print (date,"\t\t",name8,"\t\t",price8,"\t\t\t",num,"\t\t\t",num8,)
+
+id9 = 9
+date="9号"
+name9="牛仔裤"
+price9=86.3
+num=600
+num9=60
+
+print (date,"\t\t",name9,"\t\t",price9,"\t\t\t",num,"\t\t\t",num9,)
+
+id10 = 10
+date="10号"
+name10="羽绒服"
+price10=253.6
+num=500
+num10=10
+
+print (date,"\t\t",name10,"\t\t",price10,"\t\t\t",num,"\t\t\t",num10,)
+
+id11 = 11
+date="11号"
+name11="牛仔裤"
+price11=86.3
+num=600
+num11=60
+print (date,"\t\t",name11,"\t\t",price11,"\t\t\t",num,"\t\t\t",num11,)
+
+
+id12 = 12
+date="12号"
+name12="皮草"
+price12=135.9
+num=855
+num12=63
+print (date,"\t\t",name12,"\t\t",price12,"\t\t\t",num,"\t\t\t",num12,)
+
+id13 = 13
+date="13号"
+name13="T恤"
+price13=65.8
+num=632
+num13=63
+print (date,"\t\t",name13,"\t\t",price13,"\t\t\t",num,"\t\t\t",num13,)
+
+id14 = 14
+date="14号"
+name14="风衣"
+price14=96.8
+num=335
+num14=43
+print (date,"\t\t",name14,"\t\t",price14,"\t\t\t",num,"\t\t\t",num14,)
+
+id15 = 15
+date="15号"
+name15="牛仔裤"
+price15=86.3
+num=600
+num15=60
+print (date,"\t\t",name15,"\t\t",price15,"\t\t\t",num,"\t\t\t",num15,)
+
+id16 = 16
+date="16号"
+name16="T恤"
+price16=65.8
+num=632
+num16=63
+print (date,"\t\t",name16,"\t\t",price16,"\t\t\t",num,"\t\t\t",num16,)
+
+id17 = 17
+date="17号"
+name17="羽绒服"
+price17=253.6
+num=500
+num17=10
+print (date,"\t\t",name17,"\t\t",price17,"\t\t\t",num,"\t\t\t",num17,)
+
+id18 = 18
+date="18号"
+name18="风衣"
+price18=96.8
+num=335
+num18=43
+print (date,"\t\t",name18,"\t\t",price18,"\t\t\t",num,"\t\t\t",num18,)
+
+id19 = 19
+date="19号"
+name19="T恤"
+price19=65.8
+num=632
+num19=63
+print (date,"\t\t",name19,"\t\t",price19,"\t\t\t",num,"\t\t\t",num19,)
+
+id20 = 20
+date="20号"
+name20="牛仔裤"
+price20=86.3
+num=600
+num20=60
+print (date,"\t\t",name20,"\t\t",price20,"\t\t\t",num,"\t\t\t",num20,)
+
+id21 = 21
+date="21号"
+name21="皮草"
+price21=135.9
+num=855
+num21=63
+print (date,"\t\t",name21,"\t\t",price21,"\t\t\t",num,"\t\t\t",num21,)
+
+id22 = 22
+date="22号"
+name22="风衣"
+price22=96.8
+num=335
+num22=43
+print (date,"\t\t",name22,"\t\t",price22,"\t\t\t",num,"\t\t\t",num22,)
+
+id23 = 23
+date="23号"
+name23="T恤"
+price23=65.8
+num=632
+num23=63
+print (date,"\t\t",name23,"\t\t",price23,"\t\t\t",num,"\t\t\t",num23,)
+
+id24 = 24
+date="24号"
+name24="牛仔裤"
+price24=86.3
+num=600
+num24=60
+print (date,"\t\t",name24,"\t\t",price24,"\t\t\t",num,"\t\t\t",num24,)
+
+id25 = 25
+date="25号"
+name25="T恤"
+price25=65.8
+num=632
+num25=63
+print (date,"\t\t",name25,"\t\t",price25,"\t\t\t",num,"\t\t\t",num25,)
+
+id26 = 23
+date="26号"
+name26="风衣"
+price26=96.8
+num=335
+num26=43
+print (date,"\t\t",name26,"\t\t",price26,"\t\t\t",num,"\t\t\t",num26,)
+
+id27 = 27
+date="27号"
+name27="皮草"
+price27=135.9
+num=855
+num27=63
+print (date,"\t\t",name27,"\t\t",price27,"\t\t\t",num,"\t\t\t",num27,)
+
+id28 = 28
+date="28号"
+name28="羽绒服"
+price28=253.6
+num=500
+num28=10
+print (date,"\t\t",name28,"\t\t",price28,"\t\t\t",num,"\t\t\t",num28,)
+
+id29 = 29
+date="29号"
+name29="T恤"
+price29=65.8
+num=632
+num29=63
+print (date,"\t\t",name29,"\t\t",price29,"\t\t\t",num,"\t\t\t",num29,)
+
+id30 = 30
+date="30号"
+name30="风衣"
+price30=96.8
+num=335
+num30=43
+print (date,"\t\t",name30,"\t\t",price30,"\t\t\t",num,"\t\t\t",num30,)
+
+print ("总销售额为",(price1*num1+price2*num2+price3*num3+price4*num4+price5*num5+price6*num6+
+                price7*num7+price8*num8+price9*num9+price10*num10+price11*num11+price12*num12+price13*num13+
+                price14*num14+price15*num15+price16*num16+price17*num17+price18*num18+price19*num19+price20*num20+
+                price21*num21+price22*num22+price23*num23+price24*num24+price25*num25+price26*num26+price27*num27+
+                price28*num28+price29*num29+price30*num30))
+print ("羽绒服占销售总额比为",((price1*num1+price8*num8+price10*num10+price17*num17+price28*num28)/143080)*100,"%")
+print ("牛仔裤占销售总额比为",((price2*num2+price7*num7
+       +price9*num9+price11*num11+price15*num15+price20*num20+price24*num24)/143080)*100,"%")
+print ("风衣占销售总额比为",((price3*num3+price14*num14+price18*num18+price22*num22+price26*num26)/143080)*100,"%")
+print ("皮草占销售总额比为",((price4*num4+price12*num12+ price21*num21+price27*num27)/143080)*100,"%")
+print ("T恤占销售总额比为",((price5*num5+price13*num13+price16*num16
+       +price19*num19+price23*num23+price25*num2+price29*num29)/143080)*100,"%")
+print ("衬衫占销售总额比为",((price1*num1)/143080)*100,"%")
